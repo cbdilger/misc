0f15af853d908886e3492c54a2aebb6bbc3764a0
Bradley Dilger <cbdilger@gmail.com>
2020-01-07 15:34:55 -0500

    Create process-yt-captions.py
    Parse automatically generated captions copied from YouTube into transcripts.

---
+++ process-yt-captions.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin/python3
+"""
+Bradley Dilger, cbdilger@gmail.com: parse captions copied from YouTube, reduce the number of timestamps printed out, put them in brackets, and concatenate the strings involved. 
+
+00:00	all right we see red lights that's what
+00:02	we want to see all right so I printed
+00:06	out the questions okay so what we're
+00:08	gonna do is first we have some questions
+"""
+
+# make an empty hash
+Transcript = {}
+
+# start our counter
+increment = 0
+
+# higher numbers will print fewer timestamps
+timestamp = 8
+
+# read in file from standard input
+# https://stackoverflow.com/questions/1450393/how-do-you-read-from-stdin
+
+# needed for standard input
+import sys
+
+# read transcript file and build a directory with time:content as key:value pairs
+for line in sys.stdin:
+    time,content = (line.rstrip()).split('\t')
+    Transcript[time] = content
+
+# print transcript file 
+for time in Transcript.keys():
+	increment = increment + 1
+	
+	if (increment % timestamp == 0):
+		print("\n\n", '[', time, ']', sep='')
+		print(Transcript[time], end=' ')
+	else:
+		print(Transcript[time], end=' ')
+
+
+print()
